8e2ee4ff0383c3e313231e6b2e62cb2a1388a0b8
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-24 15:13:59 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,22 @@
 class Artist
   attr_accessor :name, :songs
-  @@all_songs = []
+  @@song_count = 0
   def initialize(name)
     @name = name
     @songs = []
   end
   def add_song(song)
     @songs << song
-    @@all_songs << song
+    @@song_count += 1
     song.artist = self
   end
   def add_song_by_name(song_name)
     new_song = Song.new(song_name)
     new_song.artist = self
     @songs << new_song
-    @@all_songs << new_song
+    @@song_count += 1
   end
   def self.song_count
-    @@all_songs.size
+    @@song_count
   end
 end
