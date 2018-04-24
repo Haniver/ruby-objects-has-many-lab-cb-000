9957cdaad52cd3417195ca7ab20c577963630ab8
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-24 15:09:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,4 +8,9 @@ class Artist
     @songs << song
     song.artist = self
   end
+  def add_song_by_name(song_name)
+    new_song = Song.new(song_name)
+    new_song.artist = self
+    @songs << new_song
+  end
 end
